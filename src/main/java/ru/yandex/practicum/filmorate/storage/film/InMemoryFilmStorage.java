8d067bfde3ca9c5476465e4a4cd8696688046707
manager.shambala <lachins@mail.ru>
2024-03-14 04:42:18 +0200

    Добавлена обработка исключений 2

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/InMemoryFilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/InMemoryFilmStorage.java
@@ -14,7 +14,7 @@ import java.util.Map;
 
 @Slf4j
 @Component
-public class InMemoryFilmStorage implements FilmStorage{
+public class InMemoryFilmStorage implements FilmStorage {
 
     private final Map<Integer, Film> films = new HashMap<>();
     private int id = 0;
